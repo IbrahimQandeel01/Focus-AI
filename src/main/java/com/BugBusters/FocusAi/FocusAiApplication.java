c20b967bf983dfa187000fc31fb991415288ac31
ibrahim <ibrahim.qand@gmail.com>
2025-03-17 18:33:31 +0300

    first commit

---
+++ src/main/java/com/BugBusters/FocusAi/FocusAiApplication.java
@@ -0,0 +1,14 @@
+package com.BugBusters.FocusAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusAiApplication.class, args);
+		System.out.println("hi ");
+	}
+
+}
